4268aec48ce5e223c5c6b63861c510ffc10b4539
bekzat <bekzat46@mail.ru>
2021-11-16 01:16:31 +0600

    Tablica umnojeniya

--- Example.java
+++ Example.java
@@ -1,5 +1,20 @@
 class Example{
+
     public static void main(String args[]){
-        System.out.println("Prostaya programma na Java.");
+
+        int a,b,c;
+        System.out.println("Tablica umnojeniya:");
+        System.out.println();
+        for(a=2;a<10;a++)
+        {
+            for(b=1;b<10;b++)
+            {
+                c=a*b;
+                System.out.println(a+"*"+b+"="+c);
+            }
+
+            System.out.println();
+        }
+
     }
 }
